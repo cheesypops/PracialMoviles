6950f96988e18dcb4fd88718165b6ed7bcb1f24f
Cheesypops <00030821@uca.edu.sv>
2023-05-23 09:18:46 -0600

    Fix: repository

--- app/src/main/java/com/example/banktracker/BankTrackerApplication.kt
+++ app/src/main/java/com/example/banktracker/BankTrackerApplication.kt
@@ -6,6 +6,7 @@ import com.example.banktracker.repository.BankRepository
 
 class BankTrackerApplication: Application() {
 
+    //permitir que el repositorio obtenga una variable de bancos
     val bankRepository: BankRepository by lazy {
         BankRepository(banks)
     }
--- app/src/main/java/com/example/banktracker/repository/BankRepository.kt
+++ app/src/main/java/com/example/banktracker/repository/BankRepository.kt
@@ -5,8 +5,11 @@ import com.example.banktracker.data.bankModel.BankModel
 class BankRepository(private val banks: MutableList<BankModel>) {
 
     //funciones del repositorio
+
+    //obtener los bancos
     fun getBanks() = banks
 
+    //agregar un nuevo banco
     fun addBank(bank: BankModel) {
         banks.add(bank)
     }
