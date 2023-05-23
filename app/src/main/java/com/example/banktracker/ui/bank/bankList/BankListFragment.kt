f801873fa94b32d0f487a561076b4c66869293f2
Cheesypops <00030821@uca.edu.sv>
2023-05-23 10:01:04 -0600

    Add: ListView with recyclerview active

--- app/src/main/java/com/example/banktracker/ui/bank/bankList/BankListFragment.kt
+++ app/src/main/java/com/example/banktracker/ui/bank/bankList/BankListFragment.kt
@@ -5,56 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.banktracker.R
+import com.example.banktracker.data.bankModel.BankModel
+import com.example.banktracker.databinding.BankItemBinding
+import com.example.banktracker.databinding.FragmentBankListBinding
+import com.example.banktracker.ui.bank.bankList.recycleView.BankAdapter
+import com.example.banktracker.ui.bank.bankViewModel.BankViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BankListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BankListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //creacion de variables generales
+    private lateinit var binding: FragmentBankListBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val bankViewModel: BankViewModel by activityViewModels<BankViewModel> {
+        BankViewModel.Factory
     }
+
+    private lateinit var adapter: BankAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bank_list, container, false)
+        binding = FragmentBankListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BankListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BankListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setRecyclerView(view)
+
+        binding.actionToCreateMovie.setOnClickListener(){
+            bankViewModel.clearData()
+            it.findNavController().navigate(R.id.action_bankListFragment_to_newBankFragment)
+        }
+    }
+
+    //funion para mostrar banco seleccionado
+    private fun showSelectedBank(bank: BankModel){
+        bankViewModel.setSelectedItem(bank)
+
+        findNavController().navigate(R.id.action_bankListFragment_to_bankInfoFragment)
+    }
+
+    //funcion de display
+    private fun displayBanks(){
+        adapter.setData(bankViewModel.getBanks())
+        adapter.notifyDataSetChanged()
+    }
+
+    //setear el recycle view
+    private fun setRecyclerView(view:View){
+        binding.recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        adapter = BankAdapter{bank -> showSelectedBank(bank)}
+
+        binding.recyclerView.adapter = adapter
+        displayBanks()
     }
 }
